0a7cb4fc0fd03f8cd752dfdd49d81feeb288ae4b
Shane McGowan <git@shanemcgowan.com>
2024-06-29 03:23:08 +0100

    updated styles

--- index.js
+++ index.js
@@ -20,13 +20,19 @@ async function loadFromWebsiteDaft(){
 
   // render to table
   document.querySelector(SELECTORS.daft).innerHTML = data.adverts.length;
-  document.querySelector(SELECTORS.daftLastUpdated).innerHTML = data.lastUpdated.toLocaleString();
+  document.querySelector(SELECTORS.daftLastUpdated).innerHTML = `Updated ${data.lastUpdated.toLocaleString()}`;
 
   // sort by price - highest to lowest
   let adverts = data.adverts.sort((a, b) => b.price - a.price);
 
   // render to list
   adverts.forEach((advert) => {
-    document.querySelector(SELECTORS.daftOutput).innerHTML += `<li>€${advert.price} per month - ${advert.address} <a href="${advert.url}" target="_blank">View</a></li>`;
+    document.querySelector(SELECTORS.daftOutput).innerHTML += `<li class="flex align-items-center border-bottom">
+      <span class="p-1">€${advert.price}</span> 
+      <span class="ellipsis-text">${advert.address}</span>
+      <span class="p-1 ml-auto">
+        <a href="${advert.url}" target="_blank">View</a>
+      </span>
+    </li>`;
   });
 }
